3aed8864aab8728b6ad3c5bc0939b38d4903cd65
Josh Trebilco <bitbucket@trebilco.net>
2025-01-01 19:51:20 +1300

    add basic board design

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Play', ...defaultTheme.fontFamily.sans],
             },
         },
     },
