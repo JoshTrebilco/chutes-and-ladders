ff39716d158967c508b744cba47e2d38d27eea2a
Josh Trebilco <bitbucket@trebilco.net>
2025-01-02 16:59:17 +1300

    add link back to index

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <h1 class="text-4xl font-bold text-gray-800 dark:text-white">Chutes and Ladders</h1>
+    <a href="{{ route('games.index') }}" class="text-4xl font-bold text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300">Chutes and Ladders</a>
     <div class="mt-2 lg:mt-5 flex flex-col gap-5 lg:flex-row">
         <x-board :board="$board" :game="$game" />
         <x-panel :game="$game" :auth_player="$auth_player" />
